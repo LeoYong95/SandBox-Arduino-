37699c8b6d4528b5a41e9ea8deb8d0697d1f6d2e
leoyong <y_c_l__@live.com>
2015-06-09 12:53:48 +0800

    eliminate delay command

--- LCD_screen/LCD_screen.ino
+++ LCD_screen/LCD_screen.ino
@@ -2,40 +2,49 @@
 #include <LiquidCrystal.h>
 
 
-LiquidCrystal LCD(12, 11, 5, 4, 3, 2);
+class LCD 
+{
+//---------sensor pin
+int sensor_Pin;
+//---------variables
+ unsigned long previousTime;
+ unsigned long currentTime;
+//----------Line to print
+ String line ="LEO YONG IS AWESOME";
+ 
 
-byte Heart[8] = {
-  B00000,
-  B00000,
-  B00000,
-  B11011,
-  B11111,
-  B01110,
-  B00100,
+public:
+
+LCD()
+{
+LiquidCrystal lcd(12,11,5,4,3,2);
+lcd.begin(16,2);
+previousTime = 0;
+lcd.print(line);
+lcd.scrollDisplayLeft();
 };
 
-
-void LCD_setup()
+void update()
 {
-//stearting the LCD screen.(16 collumn, 2 row)
-LCD.begin(16,2);
-char line[]= "LEO YONG IS AWESOME";
-//for loop to move text on screen
-LCD.print(line);
+   
 }
 
-void LCD_Display()
+void time()
 {
- //scroll text from right to left
- LCD.scrollDisplayLeft();
- delay(500);
+  currentTime = millis();
+  if (currentTime - previousTime >=500)
+  {
+    LCD::update();
+  }
 }
 
+
+
+};
 void setup() {
-  LCD_setup();
+ 
 }
 
 void loop() {
-  LCD_Display();
 }
 
