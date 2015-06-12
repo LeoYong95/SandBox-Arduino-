aa0b1fed891b020cb878b9519614103798e3b969
leoyong <y_c_l__@live.com>
2015-06-12 17:08:16 +0800

    add comment

--- LCD_screen/LCD_screen.ino
+++ LCD_screen/LCD_screen.ino
@@ -1,16 +1,22 @@
-
+/*
+ - This code display the fucntionality of the LiquidCrystal Librarry of Arduino.
+ - Here the code allow the LCD screen to be interupted by an AIpin.
+ - A Timer built in the micro processor is also utilised to call the update function.
+*/
 #include <LiquidCrystal.h>
+
  
- LiquidCrystal lcd(12,11,5,4,3,2);
+//list down the variables
+LiquidCrystal lcd(12,11,5,4,3,2);
  
+//create a LCD class
 class LCD 
 {
   public:
 //---------variables
-
- unsigned long previousTime;
- unsigned long currentTime;
- int sensorvalue;
+unsigned long previousTime;
+unsigned long currentTime;
+int sensorvalue;
 
 //----------Line to print
 String line;
@@ -21,14 +27,20 @@ LCD()
   
 };
 
+//----------update the lcd screen
 void update()
 {
   currentTime = millis();
+  
+  //--------only interupt for every second
   if (currentTime - previousTime >=1000)
   {
     lcd.clear();
+    
     previousTime = currentTime;
+    
     sensorvalue = analogRead(A0);
+    
     Serial.println(sensorvalue);
     
    if( sensorvalue <100)
@@ -50,25 +62,29 @@ void update()
 }
 
 };
-
+//-----------one lcd variables
 LCD screen1;
 
 void setup() {
+//----------initiate LCD screen
+
   lcd.begin(16,2);
   lcd.print("starting");
+
    Serial.begin(9600);
      
-
+//----------Timer setup.
 OCR0A = 0xAF;
 TIMSK0 |= _BV(OCIE0A);
 }
 
+//---------signal generated for every milisecond
 SIGNAL(TIMER0_COMPA_vect)
 {
   screen1.update();  
 }
   
-  
+//empty loop.  
 void loop() {
 }
 
